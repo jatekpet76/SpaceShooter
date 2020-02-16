7af51a6f0c7b76432a566f9e17c0a84beb1622c5
Godzzo <egodzzo@gmail.com>
2020-02-16 04:32:41 +0100

    First Step - moving the yellow box 😃

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0f, 0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * horizontal * speed * Time.deltaTime);
+        transform.Translate(Vector3.up * vertical * speed * Time.deltaTime);
+    }
+}
