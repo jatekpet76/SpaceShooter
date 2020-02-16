a8ef3b26d8ed12c9c1c27aa4cc7a185de8bfbd0a
Godzzo <egodzzo@gmail.com>
2020-02-16 05:06:07 +0100

    Movement boundary and starting Laser

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 3f;
+    public float speed = 3.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +15,36 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+
+
         transform.Translate(Vector3.right * horizontal * speed * Time.deltaTime);
         transform.Translate(Vector3.up * vertical * speed * Time.deltaTime);
+
+        // y < -4.1f y > 6 x >9.2 x < -4.1
+
+        if (transform.position.y < -4.1f)
+        {
+            transform.position = new Vector3(transform.position.x, 6f, 0f);
+        }
+        else if (transform.position.y > 6f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.1f, 0f);
+        }
+        else if (transform.position.x < -9.2f)
+        {
+            transform.position = new Vector3(9.2f, transform.position.y, 0f);
+        }
+        else if (transform.position.x > 9.2f)
+        {
+            transform.position = new Vector3(-9.2f, transform.position.y, 0f);
+        }
     }
 }
